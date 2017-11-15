9f91a562e7694587d56ed10b9706d7c6a2817651
Max Brauer <ma.brauer@live.de>
2017-11-15 15:52:59 +0100

    a parameter parser for startup parameters for exe files added

---
+++ MaxLib/Data/StartupParameter/ParamLoader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxLib.Data.StartupParameter
+{
+    class ParamLoader
+    {
+        public Dictionary<string, string> Options { get; private set; }
+
+        public List<string> Commands { get; private set; }
+
+        public string this[int commandIndex] => Commands[commandIndex];
+        public string this[string optionKey] => Options[optionKey];
+
+        public bool MatchCommand(params string[] command)
+        {
+            if (command.Length != Commands.Count) return false;
+            for (int i = 0; i < command.Length; ++i)
+                if (command[i].ToLower() != Commands[i].ToLower())
+                    return false;
+            return true;
+        }
+
+        public bool MatchCommandStart(params string[] command)
+        {
+            if (command.Length > Commands.Count) return false;
+            for (int i = 0; i < command.Length; ++i)
+                if (command[i].ToLower() != Commands[i].ToLower())
+                    return false;
+            return true;
+        }
+
+        public ParamLoader(string[] args)
+        {
+            Options = new Dictionary<string, string>();
+            Commands = new List<string>();
+            for (int i = 0; i<args.Length; ++i)
+            {
+                if (args[i].StartsWith("-"))
+                {
+                    var ind = args[i].IndexOf('=');
+                    if (ind == -1)
+                    {
+                        if (i < args.Length - 1 && !args[i + 1].StartsWith("-"))
+                            Options[args[i]] = Options[args[++i]];
+                        else Options[args[i]] = null;
+                    }
+                    else
+                    {
+                        var name = args[i].Remove(ind);
+                        var value = args[i].Substring(ind + 1);
+                        Options[name] = value;
+                    }
+                }
+                else Commands.Add(args[i]);
+            }
+        }
+    }
+}
