b322e8180a343f9587f3c8ef9218f9bb18d2aadf
Max Brauer <ma.brauer@live.de>
2020-06-02 11:52:09 +0200

    refactor warnings and suggestions in MaxLib.Test

--- MaxLib.Test/Tools/SolutionFinder/Problems/Sudoku/SudokuTest.cs
+++ MaxLib.Test/Tools/SolutionFinder/Problems/Sudoku/SudokuTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MaxLib.Tools.SolutionFinder.Problems.Sudoku;
+﻿using MaxLib.Tools.SolutionFinder.Problems.Sudoku;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
